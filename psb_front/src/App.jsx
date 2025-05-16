733f5eb1109107a6cd9ee794effec7281afe8c0a
Fogetik <danila041202@gmail.com>
2025-05-16 22:42:00 +0300

    asd

--- psb_front/src/App.jsx
+++ psb_front/src/App.jsx
@@ -338,37 +338,4 @@ const App = () => {
 };
 
 
-import React, {useEffect, useState} from "react";
-
-function App() {
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        const tg = window.Telegram.WebApp;
-
-        if (tg?.initDataUnsafe?.user) {
-            setUser(tg.initDataUnsafe.user);
-        } else {
-            // Telegram WebApp не инициализирован
-            console.warn('Telegram WebApp is not initialized or user not found.');
-        }
-
-        tg?.ready(); // Сообщить Telegram, что WebApp готов
-    }, []);
-
-    if (!user) {
-        return <div>Загрузка данных Telegram...</div>;
-    }
-
-    return (
-        <div >
-            <h1>Привет, {user.first_name}!</h1>
-            {user.last_name && <p>Фамилия: {user.last_name}</p>}
-            <p>Юзернейм: @{user.username}</p>
-            <p>Telegram ID: {user.id}</p>
-            {/*<img src={user.photo_url} alt="avatar" width="100" style={{ borderRadius: '50%' }} />*/}
-        </div>
-    );
-}
-
 export default App;
