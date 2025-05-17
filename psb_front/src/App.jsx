06e95facbcaadb334aa55cd6eeff72b7350cfe8f
Fogetik <danila041202@gmail.com>
2025-05-17 21:16:22 +0300

    asd

--- psb_front/src/App.jsx
+++ psb_front/src/App.jsx
@@ -241,18 +241,6 @@ export const App = () => {
                                 >
                                     Мои заявки
                                 </button>
-                                {editId && (
-                                    <button
-                                        type="button"
-                                        className="btn btn-secondary ms-2"
-                                        onClick={() => {
-                                            setEditId(null);
-                                            setError(null);
-                                        }}
-                                    >
-                                        Отмена
-                                    </button>
-                                )}
                             </div>
                         </form>
                     </div>
@@ -333,6 +321,7 @@ export const App = () => {
                                                 className="btn btn-secondary ms-2"
                                                 onClick={() => {
                                                     setEditId(null);
+                                                    setEditIdAdditional(null);
                                                     setError(null);
                                                 }}>
                                                 Отмена
@@ -395,6 +384,17 @@ export const App = () => {
 
 
                                         ))}
+
+                                    <div className="text-center mt-4">
+
+                                        <button
+                                            className="btn btn-primary ms-2"
+                                            type="button"
+                                            onClick={() => setActive("left")}
+                                        >
+                                            Создание заявки
+                                        </button>
+                                    </div>
                                 </div>
                             )
 
